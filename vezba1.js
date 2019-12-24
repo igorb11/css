8d984c562bf9fab2d0b508833e896d086ede0d11
igorb11 <58231242+igorb11@users.noreply.github.com>
2019-12-24 21:57:28 +0100

    Add files via upload

---
+++ vezba1.js
@@ -0,0 +1,89 @@
+// var grade=55;
+// var passed=false;
+// if(grade>=60){
+// 	console.log("Polozen")}
+// 	else if (grade<60){
+// 		console.log("Ne Polozen")}
+// 		else if (passed==True){
+// 			console.log("Polozen")}
+// 			else{
+// 				console.log("Vneste povtorno poeni")
+// 			}
+
+
+
+// var d=new Date();
+// var mesec=d.getMonth();
+// if (mesec>=3) {console.log("Zima")}
+// else if (mesec=4,5,6){console.log("Prolet")}
+// else if (mesec=7,8,9){console.log("Leto")}
+// else if (mesec=10,11,12){console.log("Esen") }
+// else {
+// 	console.log("Greska")
+// }
+// console.log(mesec);
+
+// var d=new Date();
+// var den=d.getDay();
+
+// switch(den){
+// case 0:
+// console.log ("Nedela")
+// break;
+// case 1:
+// console.log ("Ponedelnik")
+// break;
+// case 2:
+// console.log ("Vtornik")
+// break;
+// case 3:
+// console.log ("Sreda")
+// break;
+// case 4:
+// console.log ("Cetvrtok")
+// break;
+// case 5:
+// console.log ("Petok")
+// break;
+// case 6:
+// console.log ("Sabota")
+// break;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+	
